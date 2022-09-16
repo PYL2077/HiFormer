7c4fc0d144035a08a984b7939c013770666d9bcb
PYL2077 <91117953+PYL2077@users.noreply.github.com>
2022-09-16 15:37:24 +0800

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,16 @@
+# ----------------------------------------------------------------------------------------------
+# CoFormer Official Code
+# Copyright (c) Junhyeong Cho. All Rights Reserved 
+# Licensed under the Apache License 2.0 [see LICENSE for details]
+# ----------------------------------------------------------------------------------------------
+# Modified from DETR (https://github.com/facebookresearch/detr)
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved [see LICENSE for details]
+# ----------------------------------------------------------------------------------------------
+
+from .gsrformer import build_gsrformer_encoder
+from .gsrformer import build_gsrformer_decoder
+
+def build_encoder_model(args):
+    return build_gsrformer_encoder(args)
+def build_decoder_model(args):
+    return build_gsrformer_decoder(args)
